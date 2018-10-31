e650e63d7ad5c61938cd453c25def28867614edb
lyndoo <lyndoo@qq.com>
2018-10-31 22:55:56 +0800

    update

--- app/scan/index.py
+++ app/scan/index.py
@@ -4,12 +4,12 @@ import requests
 from app import Sign
 from main import cache
 
-# AppId = 'wx9c05576f1b274ddf'
-# app_Secret = 'e5ebc637339dc0190c9f28ade87c89f6'
+AppId = 'wx9c05576f1b274ddf'
+app_Secret = 'e5ebc637339dc0190c9f28ade87c89f6'
 
 #testappid
-AppId = 'wx0740bc697491a6fd'
-app_Secret = '566721fef2289a59020795c5deb8bce6'
+# AppId = 'wx0740bc697491a6fd'
+# app_Secret = '566721fef2289a59020795c5deb8bce6'
 
 @scan.route('/')
 def index():
@@ -29,7 +29,7 @@ def getWxData(url):
     sign = Sign.Sign(jsp_api_tikcet,url)
     return sign.sign()
 
-@cache.memoize(timeout=7000)
+#@cache.memoize(timeout=7000)
 def create_AccessToken():
     url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+AppId+"&secret="+ app_Secret  # 这里是需要获取的网页
     res = requests.get(url)  # 使用urllib模块获取网页内容
@@ -38,7 +38,7 @@ def create_AccessToken():
     return content['access_token']
 
 
-# @cache.memoize(timeout=7200)
+#@cache.memoize(timeout=7000)
 def create_JsApiTicket(accesstoken):
     url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+accesstoken+'&type=jsapi'  # 这里是需要获取的网页
     res = requests.get(url)  # 使用urllib模块获取网页内容
